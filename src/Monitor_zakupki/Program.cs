cafea08c76ddfe997cfa6a2ad9f8f625163c9ba2
alast0r <efanov.vladisla6@gmail.com>
2026-05-20 12:01:34 +0300

    Приведя Json и классы в program к единому стилю PascalCase

--- src/Monitor_zakupki/Program.cs
+++ src/Monitor_zakupki/Program.cs
@@ -5,3 +5,25 @@ builder.Services.AddHostedService<Worker>();
 
 var host = builder.Build();
 host.Run();
+
+class UserSettings
+{
+    public string[] InnList {  get; set; }
+    public string NotificationEmail { get; set; }
+    public int IntervalHours { get; set; }
+}
+
+class MainSettings
+{
+    public string PathToLog { get; set; }
+}
+
+class EmailSettings
+{
+    public string SmtpServer { get; set; }
+    public int SmtpPort { get; set; }
+    public string SmtpLogin { get; set; }
+    public string SmtpPassword { get; set; }
+    public string SmtpFrom { get; set; }
+
+}
