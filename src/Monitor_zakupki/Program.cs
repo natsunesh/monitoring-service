c80c580e54e1ab6ceba19891f7d14994ad643fa6
alast0r <efanov.vladisla6@gmail.com>
2026-05-20 18:01:42 +0300

    Иницилизация UserSettings, MainSettings

--- src/Monitor_zakupki/Program.cs
+++ src/Monitor_zakupki/Program.cs
@@ -3,27 +3,16 @@ using Monitor_zakupki;
 var builder = Host.CreateApplicationBuilder(args);
 builder.Services.AddHostedService<Worker>();
 
+builder.Services.Configure<UserSettings>(
+    builder.Configuration.GetSection("UserSettings"));
+
+builder.Services.Configure<MainSettings>(
+    builder.Configuration.GetSection("MainSettings"));
+
+
 var host = builder.Build();
 host.Run();
 
-class UserSettings
-{
-    public string[] InnList {  get; set; }
-    public string NotificationEmail { get; set; }
-    public int IntervalHours { get; set; }
-}
 
-class MainSettings
-{
-    public string PathToLog { get; set; }
-}
 
-class EmailSettings
-{
-    public string SmtpServer { get; set; }
-    public int SmtpPort { get; set; }
-    public string SmtpLogin { get; set; }
-    public string SmtpPassword { get; set; }
-    public string SmtpFrom { get; set; }
 
-}
