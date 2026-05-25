e072a938b7b4efbf7b1b5b17fb29ae1466f1ab5c
alast0r <efanov.vladisla6@gmail.com>
2026-05-25 16:57:52 +0300

    Добавление относительного пути к config/app-config.json для стабильной работы

--- src/Monitor_zakupki/Program.cs
+++ src/Monitor_zakupki/Program.cs
@@ -3,37 +3,23 @@ using Monitor_zakupki.Models;
 using Monitor_zakupki.Interfaces;
 using Monitor_zakupki.Services;
 
-// Создаём builder для приложения-хоста worker-службы
 var builder = Host.CreateApplicationBuilder(new HostApplicationBuilderSettings
 {
-    // Указываем корень проекта, чтобы JSON и другие файлы читались из нужной папки
-    ContentRootPath = @"L:\\программирование\\monitoring_service"
+    ContentRootPath = AppContext.BaseDirectory
 });
 
-// Регистрируем фонового worker-а, который будет запускаться как hosted service
-builder.Services.AddHostedService<Worker>();
-
-// Подключаем JSON-конфиг приложения
-// reloadOnChange: true позволяет подхватывать изменения файла без перезапуска
 builder.Configuration.AddJsonFile("config/app-config.json", optional: false, reloadOnChange: true);
 
-// Регистрируем сервис уведомлений
-builder.Services.AddTransient<INotificationService, NotificationService>();
+builder.Services.AddHostedService<Worker>();
 
-// Регистрируем сервис парсинга закупок
+builder.Services.AddTransient<INotificationService, NotificationService>();
 builder.Services.AddTransient<IProcurementParserService, FakeProcurementParserService>();
 
-// Привязываем секцию UserSettings из JSON к классу UserSettings
 builder.Services.Configure<UserSettings>(
     builder.Configuration.GetSection("UserSettings"));
 
-// Привязываем секцию MainSettings из JSON к классу MainSettings
 builder.Services.Configure<MainSettings>(
     builder.Configuration.GetSection("MainSettings"));
 
 var host = builder.Build();
 host.Run();
-
-
-
-
